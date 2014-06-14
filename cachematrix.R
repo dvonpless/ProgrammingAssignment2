93405bdc39d006c1389bca185f74dbcf29f3e88f
dvonpless <dvonpless@yahoo.com>
2014-06-13 22:31:33 -0400

    Completed assignment
    Tested the two required functions, makeCacheMatrix and cacheSolve on
    several invertible matrices.

--- cachematrix.R
+++ cachematrix.R
@@ -1,40 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## cachematrix.R
+
+## This file contains functions two functions to create an object to 
+## store a matrix and cache its inverse.
 
 
-makeVector <- function(x = numeric()) {
-    m <- NULL
-    set <- function(y) {
-        x <<- y
-        m <<- NULL
-    }
-    get <- function() x
-    setmean <- function(mean) m <<- mean
-    getmean <- function() m
-    list(set = set, get = get,
-         setmean = setmean,
-         getmean = getmean)
-}
-cachemean <- function(x, ...) {
-    m <- x$getmean()
-    if(!is.null(m)) {
-        message("getting cached data")
-        return(m)
-    }
-    data <- x$get()
-    m <- mean(data, ...)
-    x$setmean(m)
-    m
-}
-## Write a short comment describing this function
+
+## makeCacheMatrix() -- function to create a list object to store a
+## matrix object and store the inverse of the matrix.  We assume
+## that any square matrix is invertible.  The function could be made 
+## more robust by verifying that the supplied matrix object is square.
+
 makeCacheMatrix <- function(x = matrix()) {
-
+    I <- NULL
+    set<-function(y) {
+        x <<- y
+        I <<- NULL
+    }
+    get<-function() x
+    setInverse<-function(inverse) I <<- inverse
+    getInverse<-function() I
+    list(set = set, get = get,
+         setInverse = setInverse, getInverse=getInverse)
 }
 
-
-## Write a short comment describing this function
+## cacheSolve() -- function to calculate the inverse of a matrix via
+## the R solve() function.  To optimize speed, a cached version of the
+## matrix inverse will be returned if it has been calculated previously.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+ 
+ I<-x$getInverse()
+    if(!is.null(I)) {
+        message("getting cached inverse")
+        return(I)
+    }
+    data<-x$get()
+    b<-solve(data)
+    x$setInverse(b)
+    b
 }
-
